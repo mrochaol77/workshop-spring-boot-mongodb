69fabe11ad115e87c904daa6bb609d0e52aaa541
mrochaol <mrochaol@gmail.com>
2025-03-21 15:27:38 -0300

    Add comments on Posts

--- src/main/java/com/olecram/workshopmongo/domain/Post.java
+++ src/main/java/com/olecram/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.olecram.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.olecram.workshopmongo.dto.AuthorDTO;
+import com.olecram.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {		
 	}
@@ -69,6 +74,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
